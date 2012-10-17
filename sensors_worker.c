331d218ccda50b7f6d605d2559378eeab7513854
David Frey <david.frey@sensirion.com>
2012-10-17 15:10:21 +0200

    Fix sensors_worker for big delays.
    This patch fixes sensors_worker for delays greater than or equal to one
    second.
    
    Signed-off-by: David Frey <david.frey@sensirion.com>

--- sensors_worker.c
+++ sensors_worker.c
@@ -29,9 +29,8 @@ static void sensor_nano_sleep(int64_t utime)
 	if (utime <= 0)
 		return;
 
-	memset(&t, 0, sizeof(t));
-	t.tv_nsec = (long)(utime);
-
+	t.tv_sec = (time_t)(utime / 1e9);
+	t.tv_nsec = (long)(utime % 1000000000L);
 	nanosleep(&t, NULL);
 }
 
