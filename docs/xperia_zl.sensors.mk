5076a68e04f1c7c19fb47bbde91109c21ab0538a
Oskar Andero <oskar.andero@sonymobile.com>
2013-02-27 13:26:59 +0100

    Add sensor configuration files for Xperia ZL
    Signed-off-by: Oskar Andero <oskar.andero@sonymobile.com>

---
+++ docs/xperia_zl.sensors.mk
@@ -0,0 +1,11 @@
+#
+# This is the DASH configuration makefile for Xperia ZL. It should preferably be
+# included from your device configuration make files.
+#
+
+SOMC_CFG_SENSORS_GYRO_MPU3050 := yes
+SOMC_CFG_SENSORS_SYSTEM_WIDE_ALS := yes
+SOMC_CFG_SENSORS_PROXIMITY_APDS9702 := yes
+SOMC_CFG_SENSORS_ACCEL_BMA250NA_INPUT := yes
+SOMC_CFG_SENSORS_COMPASS_AK8963 := yes
+
