9f344f210e8962b10187d825541775ebf2a0ac22
Alin Jerpelea <jerpelea@gmail.com>
2013-03-01 17:22:07 +0200

    libals: : add dummy shared lib
    Signed-off-by: Alin Jerpelea <jerpelea@gmail.com>

---
+++ libs/libals/libals_dummy.c
@@ -0,0 +1,32 @@
+#include <linux/types.h>
+#include <hardware/sensors.h>
+
+int als_getlux(float *data)
+{
+    return 0;
+}
+
+int als_get_filtered(unsigned long *data)
+{
+    return 0;
+}
+
+int als_getlux_filtered(float *data)
+{
+    return 0;
+}
+
+int als_enable(void)
+{
+    return 0;
+}
+
+int als_disable(void)
+{
+    return 0;
+}
+
+int als_available(void)
+{
+    return 0;
+}
