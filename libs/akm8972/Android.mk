32a7358a3d9ec2bb293555a3e0d7874e86f69b9e
jerpelea <jerpelea@gmail.com>
2013-03-22 11:39:05 +0200

    Update Android.mk

--- libs/akm8972/Android.mk
+++ libs/akm8972/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(SOMC_CFG_SENSORS_COMPASS_AK8972),yes) 
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -6,3 +8,5 @@ LOCAL_MODULE := libsensors_akm8972
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES += libsensors_akm8972_dummy.c
 include $(BUILD_SHARED_LIBRARY)
+
+endif
